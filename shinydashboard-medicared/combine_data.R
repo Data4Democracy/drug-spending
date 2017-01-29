5505e1e0ef5f08a0a4f44e50a50e40d99a359fba
Jennifer Thompson <thompson.jennifer@gmail.com>
2017-01-29 16:09:12 -0600

    Add ALL BRAND NAMES row for each generic to testing data sets

--- shinydashboard-medicared/combine_data.R
+++ shinydashboard-medicared/combine_data.R
@@ -15,6 +15,10 @@ data_list <- data.world::query(conn,
                                dataset = 'data4democracy/drug-spending',
                                query = "SELECT * FROM Tables")
 
+drug_uses <- data.world::query(conn,
+                               dataset = 'data4democracy/drug-spending',
+                               query = "SELECT * FROM drug_uses")
+
 ## -- Read in each year's data set, add year and drug names, combine into a single data.frame ------
 ## Function to add a column with spending year to a data frame
 add_drug_year <- function(df, yr){
@@ -34,6 +38,25 @@ drug_years <- 2011:2015
 spend <- map(drug_years, get_year) %>%
   map2(drug_years, add_drug_year) %>%
   bind_rows()
+
+## -- Add a row for each generic with overall summaries of each variable ---------------------------
+spend_overall <- spend %>%
+  group_by(drugname_generic, year) %>%
+  summarise(claim_count = sum(claim_count, na.rm = TRUE),
+            total_spending = sum(total_spending, na.rm = TRUE),
+            user_count = sum(user_count, na.rm = TRUE),
+            unit_count = sum(unit_count, na.rm = TRUE),
+            user_count_non_lowincome = sum(user_count_non_lowincome, na.rm = TRUE),
+            user_count_lowincome = sum(user_count_lowincome, na.rm = TRUE)) %>%
+  mutate(total_spending_per_user = total_spending / user_count,
+         drugname_brand = "ALL BRAND NAMES",
+         ## Add NA values for variables that are brand-specific
+         unit_cost_wavg = NA,
+         out_of_pocket_avg_lowincome = NA,
+         out_of_pocket_avg_non_lowincome = NA) %>%
+  ungroup()
+
+spend <- bind_rows(spend, spend_overall)
 
 ## -- For testing: data set of 100 random generics -------------------------------------------------
 drug_list <- unique(spend$drugname_generic)
