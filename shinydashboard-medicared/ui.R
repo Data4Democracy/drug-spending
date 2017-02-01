bf486623e86de96592f368b59f044da171f52c19
Jennifer Thompson <thompson.jennifer@gmail.com>
2017-01-31 19:41:23 -0600

    Sort list of drug options and make default first alphabetical choice. Use headers for box titles rather than embedding in ggplotly objects.

--- shinydashboard-medicared/ui.R
+++ shinydashboard-medicared/ui.R
@@ -9,7 +9,8 @@ header <- dashboardHeader(title = 'Medicare Part D Users and Spending, 2011-2015
 sidebar <- dashboardSidebar(
   selectInput(inputId = 'drug',
               label = 'Choose generic drug name:',
-              choices = drug_costs$drugname_generic),
+              choices = sort(unique(drug_costs$drugname_generic)),
+              selectize = FALSE),
   sidebarMenu(menuItem("People", tabName = "people", icon = icon("user", lib = "glyphicon")),
               menuItem("Spending", tabName = "spending", icon = icon("usd", lib = "glyphicon"))))
 
@@ -21,19 +22,33 @@ body <- dashboardBody(
             p("Black lines represent overall values for the selected generic; colored lines represent values for individual brands."),
             p("As one example, a black line would represent the total for all formulations of metformin HCL; one colored line on the same chart represents only Glucophage XR."),
             fluidRow(
-              box(plotlyOutput("userCounts")),
-              box(plotlyOutput("avgClaimsPerUser"))
+              box(
+                h3("Total Number of Users by Year"),
+                plotlyOutput("userCounts")
+                ),
+              box(
+                h3("Average Claims Per User by Year"),
+                plotlyOutput("avgClaimsPerUser")
+                )
             )),
     tabItem(tabName = 'spending',
             h2(textOutput("spending_headline")),
             p("Black lines represent overall values for the selected generic; colored lines represent values for individual brands."),
             p("As one example, a black line would represent the total for all formulations of metformin HCL; one colored line on the same chart represents only Glucophage XR."),
             fluidRow(
-              box(plotlyOutput("totalSpending")),
-              box(plotlyOutput("avgCostPerUser"))
+              box(
+                h3("Total Spending by Year, US Dollars"),
+                plotlyOutput("totalSpending")
+                ),
+              box(
+                h3("Average Cost Per User by Year, US Dollars"),
+                plotlyOutput("avgCostPerUser")
+                )
             ),
             fluidRow(
-              box(plotlyOutput("oop"), width = 12)
+              box(
+                h3("Average Out of Pocket Cost Per User by Year, US Dollars"),
+                plotlyOutput("oop"), width = 12)
             ),
             p("Because out-of-pocket costs are specific to individual brands, no overall out-of-pocket cost for a single generic is provided.")
             )
