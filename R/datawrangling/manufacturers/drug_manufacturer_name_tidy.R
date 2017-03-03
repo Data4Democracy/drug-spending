4d94e3a1c73e931d8aa644330230c480a2cf938b
Stephanie Kirmer <skirmer@uchicago.edu>
2017-03-03 16:12:47 -0600

    Made improvements to name matches

--- R/datawrangling/manufacturers/drug_manufacturer_name_tidy.R
+++ R/datawrangling/manufacturers/drug_manufacturer_name_tidy.R
@@ -14,17 +14,24 @@ drugs$LABELER.NAME <- toupper(drugs$LABELER.NAME)
 
 drugs$LABELER.NAME <- ifelse(grepl("abbvie",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "ABBVIE US, LLC", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("ACCORD",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "ACCORD HEALTHCARE INC", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("ACORDA",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "ACORDA THERAPEUTICS", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("ACTAVIS",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "ACTAVIS INC", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("AKORN",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "AKORN", drugs$LABELER.NAME)
+
+drugs$LABELER.NAME <- ifelse(grepl("AMERICAN REGENT",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "AMERICAN REGENT INC", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("APOPHARMA",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "APOPHARMA", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("APTALIS",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "APTALIS PHARMA US", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("AUXILIUM",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "AUXILIUM PHARMACEUTICALS", drugs$LABELER.NAME)
+
+drugs$LABELER.NAME <- ifelse(grepl("BD RX",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "BD RX", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("CARACO",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "CARACO PHARMACEUTICAL LABORATORIES", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("EYETECH",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "EYETECH", drugs$LABELER.NAME)
 
 drugs$LABELER.NAME <- ifelse(grepl("AMGEN",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "AMGEN USA, INC", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("AMNEAL",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "AMNEAL PHARMACEUTICALS, LLC", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("AMPHASTAR",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "AMPHASTAR PHARMACEUTICALS, INC", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("ASTRAZENECA",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "ASTRAZENECA PHARMACEUTICALS", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("BAUSCH & LOMB",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "BAUSCH & LOMB", drugs$LABELER.NAME)
-
-drugs$LABELER.NAME <- ifelse(grepl("ASTRAZENECA",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "ASTRAZENECA PHARMACEUTICALS", drugs$LABELER.NAME)
-drugs$LABELER.NAME <- ifelse(grepl("ASTRAZENECA",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "ASTRAZENECA PHARMACEUTICALS", drugs$LABELER.NAME)
 
 drugs$LABELER.NAME <- ifelse(grepl("BAXALTA",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "BAXALTA US INC.", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("BAXTER",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "BAXTER HEALTHCARE CORPORATION", drugs$LABELER.NAME)
@@ -50,6 +57,11 @@ drugs$LABELER.NAME <- ifelse(grepl("DYAX",drugs$LABELER.NAME, ignore.case = TRUE
 drugs$LABELER.NAME <- ifelse(grepl("ENDO",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "ENDO PHARMACEUTICALS VALERA INC", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("FERRING",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "FERRING PHARMACEUTICALS INC", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("FRESENIUS",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "FRESENIUS KABI USA LLC", drugs$LABELER.NAME)
+
+drugs$LABELER.NAME <- ifelse(grepl("FOREST LABORATORIES",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "FOREST LABORATORIES", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("GLENMARK",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "GLENMARK PHARMACEUTICALS", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("HOSPIRA",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "HOSPIRA INC", drugs$LABELER.NAME)
+
 drugs$LABELER.NAME <- ifelse(grepl("GENZYME",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "GENZYME CORPORATION", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("GREENSTONE",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "GREENSTONE LLC", drugs$LABELER.NAME)
 
@@ -71,6 +83,12 @@ drugs$LABELER.NAME <- ifelse(grepl("MERZ",drugs$LABELER.NAME, ignore.case = TRUE
 drugs$LABELER.NAME <- ifelse(grepl("MYLAN",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "MYLAN PHARMACEUTICALS, INC", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("NOVARTIS",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "NOVARTIS", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("NOVO NORDISK",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "NOVO NORDISK, INC", drugs$LABELER.NAME)
+
+drugs$LABELER.NAME <- ifelse(grepl("NOVAPLUS",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "NOVAPLUS", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("OTSUKA",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "OTSUKA AMERICA", drugs$LABELER.NAME)
+drugs$LABELER.NAME <- ifelse(grepl("PHOTOCURE",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "PHOTOCURE INC", drugs$LABELER.NAME)
+
+drugs$LABELER.NAME <- ifelse(grepl("MEDICINES COMPANY",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "MEDICINES COMPANY, THE", drugs$LABELER.NAME)
 
 drugs$LABELER.NAME <- ifelse(grepl("OMEROS",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "OMEROS CORPORATION", drugs$LABELER.NAME)
 drugs$LABELER.NAME <- ifelse(grepl("PAR ",drugs$LABELER.NAME, ignore.case = TRUE) == TRUE, "PAR PHARMACEUTICAL", drugs$LABELER.NAME)
@@ -108,7 +126,7 @@ drugs$LABELER.NAME <- ifelse(grepl("ZIMMER",drugs$LABELER.NAME, ignore.case = TR
 
 drugs <- unique(drugs)
 
-write.csv(drugs[,c(2:14)], "U://Medicaid_Drug/drugdata_clean.csv")
+write.csv(drugs[,c(2:14)], "U://Medicaid_Drug/manufacturers_drugs_cleaned.csv")
 
 
 
